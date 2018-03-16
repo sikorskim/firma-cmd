10da93afb3b54ddc509e8668d118d9fb1eb38a5c
Maciej Sikorski <donsikorski@gmail.com>
2018-03-16 15:16:00 +0100

    minor changes

--- firma-cmd/Classes/Contractor.cs
+++ firma-cmd/Classes/Contractor.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Xml.Linq;
 
 namespace jpk_check
 {
     public class Contractor : JPKpodmiot
     {
+        public Contractor()
+        { }
+
         public Contractor(string nip, string name, string countryCode, string voivodeship, string county, string community, string city, string street, string buldingNo, string postalCode, string postOffice)
         {
             this.nip = nip;
@@ -24,10 +29,15 @@ namespace jpk_check
         public bool add()
         {
             string path = "contractors.xml";
-            
-            XDocument doc = XDocument.Load(path);
-            XElement root = doc.Element("Contractors");
-            
+
+            if (!File.Exists(path))
+            {
+                XDocument newDoc = new XDocument();
+                XElement newRoot = new XElement("Contractors");
+                newDoc.Add(newRoot);
+                newDoc.Save(path);
+            }
+
             XElement contractor = new XElement("Contractor");
             contractor.Add(new XAttribute("NIP", nip));
             contractor.Add(new XAttribute("FullName", pelnaNazwa));
@@ -40,12 +50,30 @@ namespace jpk_check
             contractor.Add(new XAttribute("BuldingNo", nrDomu));
             contractor.Add(new XAttribute("PostalCode", kodPocztowy));
             contractor.Add(new XAttribute("PostOffice", poczta));
-            
+
+
+            XElement root = XElement.Load(path);
             root.Add(contractor);
-            doc.Add(root);
-            doc.Save(path);
+            root.Save(path);
 
             return true;
+        }
+
+        public List<Contractor> getAll()
+        {
+            string path = "contractors.xml";
+            List<Contractor> list = new List<Contractor>();
+
+            XDocument doc = XDocument.Load(path);
+            foreach (XElement elem in doc.Element("Contractors").Elements("Contractor"))
+            {
+                Contractor contractor = new Contractor();
+                contractor.pelnaNazwa=elem.Attribute("FullName").Value;
+                list.Add(contractor);
+                Console.WriteLine(contractor.pelnaNazwa);
+            }
+
+            return list;
         }
     }
 }
--- firma-cmd/Program.cs
+++ firma-cmd/Program.cs
@@ -6,11 +6,15 @@ namespace firma_cmd
     {
         public static void Main(string[] args)
         {
-            Contractor c = new Contractor("777666555", "Januszex", "PL", "Wielkopolska", "śremski", "Śrem", "Śrem",
-                "Kilińskiego", "1", "63-100", "Śrem");
-//            Contractor c = new Contractor("777666444", "Adamex", "PL", "Wielkopolska", "śremski", "Śrem", "Śrem",
-//                "Wojska Polskiego", "12/1", "63-100", "Śrem");
-//            c.add();
+            ////Contractor c = new Contractor("777666555", "Januszex", "PL", "Wielkopolska", "śremski", "Śrem", "Śrem",
+            ////    "Kilińskiego", "1", "63-100", "Śrem");
+            //Contractor c = new Contractor("777666444", "Adamex", "PL", "Wielkopolska", "śremski", "Śrem", "Śrem",
+            //    "Wojska Polskiego", "12/1", "63-100", "Śrem");
+            //c.add();
+
+            Contractor c = new Contractor();
+            c.getAll();
+            Cons
         }
     }
 }
