5de914a79cae449c47658a69e0f9968959fac1a9
Maciej Sikorski <donsikorski@gmail.com>
2018-05-21 14:42:58 +0200

    update Bootstrap to version 4

--- firma-mvc/Migrations/20180521123153_migr2.Designer.cs
+++ firma-mvc/Migrations/20180521123153_migr2.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace firmamvc.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180521123153_migr2")]
+    partial class migr2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- firma-mvc/Models/Contractor.cs
+++ firma-mvc/Models/Contractor.cs
@@ -11,17 +11,33 @@ namespace firma_mvc
     {
         public int Id { get; set; }
         public string NIP { get; set; }
+        [DisplayName("Pełna nazwa")]
         public string FullName { get; set; }
+        [DisplayName("Nazwa")]
         public string Name { get; set; }
+        [DisplayName("Kod kraju")]
         public string CountryCode { get; set; }
+        [DisplayName("Województwo")]
         public string Voivodeship { get; set; }
+        [DisplayName("Powiat")]
         public string County { get; set; }
+        [DisplayName("Gmina")]
         public string Community { get; set; }
+        [DisplayName("Miejscowość")]
         public string City { get; set; }
+        [DisplayName("Ulica")]
         public string Street { get; set; }
+        [DisplayName("Nr budynku")]
         public string BuldingNo { get; set; }
+        [DisplayName("Kod pocztowy")]
         public string PostalCode { get; set; }
+        [DisplayName("Poczta")]
         public string PostOffice { get; set; }
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+        [DisplayName("Telefon")]
+        public string Phone { get; set; }
+
 
         public Contractor()
         {
