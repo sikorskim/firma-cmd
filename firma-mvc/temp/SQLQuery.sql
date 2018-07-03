7c79a5c79d8818910af36cab913e2d638cbe2d36
[Maciej Sikorski] <donsikorski@gmail.com>
2018-07-03 20:16:05 +0200

    VatRegisterBuy half VAT jquery improved

--- firma-mvc/temp/SQLQuery.sql
+++
@@ -1,6 +0,0 @@
-﻿insert into PaymentMethod values
-(7, 'przelew 7 dni'),
-(14, 'przelew 14 dni'),
-(30, 'przelew 30 dni'),
-(0, 'gotówka')
-select * from PaymentMethod
--- firma-mvc/wwwroot/js/site.js
+++ firma-mvc/wwwroot/js/site.js
@@ -1,1 +1,8 @@
-﻿
+﻿// count 50% of VAT tax on VATRegisterBuy create view
+$('#btnVat50').click(function () {
+    var vat = $('#TaxDeductibleValue').val();
+    vat=vat.replace(',','.')
+    vat = vat / 2;
+    vat = vat.toFixed(2);
+        $('#TaxDeductibleValue').val(vat);
+});
