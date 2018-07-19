9766aefc16de8e997aa29a94937deea41412908c
[Maciej Sikorski] <donsikorski@gmail.com>
2018-07-19 22:14:57 +0200

    JPKViewModel added for JPK generator view

--- firma-mvc/Controllers/JPKController.cs
+++ firma-mvc/Controllers/JPKController.cs
@@ -23,7 +23,35 @@ namespace firma_mvc.Controllers
             ViewData["Month"] = new SelectList(Tools.getMonthsDictionary(), "Key", "Value");
             ViewData["Year"] = new SelectList(Tools.getYearsList());
             ViewData["JPKType"] = new SelectList(Tools.getJPKtypes(), "Key", "Value");
-            return View();
+            JPKViewModel jpkViewModel = new JPKViewModel();
+            jpkViewModel.Month = DateTime.Now.Month - 1;
+            jpkViewModel.Year = DateTime.Now.Year;
+            return View(jpkViewModel);
+        }
+
+        // POST: Items/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index([Bind("JPKTypeId,Year,Month")] JPKViewModel jpkViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (jpkViewModel.JPKTypeId == 1)
+                {
+                    JPK_VAT jpk_vat = new JPK_VAT(jpkViewModel.Month, jpkViewModel.Year, _context);
+                    string xmlFilename = jpk_vat.generate();
+                    //await Task.Delay(1000);
+                    return RedirectToAction("GetXmlFile", new { filename = xmlFilename });
+                }
+            }
+
+            ViewData["Month"] = new SelectList(Tools.getMonthsDictionary(), "Key", "Value");
+            ViewData["Year"] = new SelectList(Tools.getYearsList());
+            ViewData["JPKType"] = new SelectList(Tools.getJPKtypes(), "Key", "Value");
+
+            return View(jpkViewModel);
         }
 
         // GET: JPK/Generate
---
+++ firma-mvc/Models/JPK_VAT/JPKViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace firma_mvc
+{
+    public class JPKViewModel
+    {
+        public int JPKTypeId { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }        
+    }
+}
