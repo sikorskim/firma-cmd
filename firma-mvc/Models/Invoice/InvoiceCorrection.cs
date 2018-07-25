97a67c886d695501203a744af9e5923df06e05fd
Maciej Sikorski <donsikorski@gmail.com>
2018-07-25 14:05:23 +0200

    InvoiceCorrections layout changed

--- firma-mvc/Models/Invoice/InvoiceCorrection.cs
+++ firma-mvc/Models/Invoice/InvoiceCorrection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace firma_mvc
     public class InvoiceCorrection : Invoice
     {
         public int InvoiceId { get; set; }
+        [DisplayName("Data korekty")]
         public DateTime DateOfCorrection { get; set; }
+        [DisplayName("Powód korekty")]
         public string CorrectionCause { get; set; }
     }
 }
