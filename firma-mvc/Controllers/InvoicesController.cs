e54adfa840fd19d2062e6f99363a9a96ed616af1
Maciej Sikorski <donsikorski@gmail.com>
2018-08-08 10:37:40 +0200

    Invoice SetPaid action added

--- firma-mvc/Controllers/InvoicesController.cs
+++ firma-mvc/Controllers/InvoicesController.cs
@@ -10,16 +10,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
-namespace firma_mvc.Controllers {
-    public class InvoicesController : Controller {
+namespace firma_mvc.Controllers
+{
+    public class InvoicesController : Controller
+    {
         private readonly ApplicationDbContext _context;
 
-        public InvoicesController (ApplicationDbContext context) {
+        public InvoicesController (ApplicationDbContext context)
+        {
             _context = context;
         }
 
         // GET: Invoice
-        public async Task<IActionResult> Index (string searchQuery, int? year, int? month, int? statusId) {
+        public async Task<IActionResult> Index (string searchQuery, int? year, int? month, int? statusId)
+        {
             ViewData["Month"] = new SelectList (Tools.getMonthsDictionary (), "Key", "Value", DateTime.Now.Month);
             ViewData["Year"] = new SelectList (Tools.getYearsList (), DateTime.Now.Year);
             ViewData["Status"] = new SelectList (_context.InvoiceStatus.ToList (), "Id", "Name");
@@ -35,47 +39,59 @@ namespace firma_mvc.Controllers {
 
             var applicationDbContext = _context.Invoice.Include (i => i.Contractor).Include (i => i.PaymentMethod).Include (i => i.InvoiceItems).Include (i => i.InvoiceStatus);
 
-            if (!String.IsNullOrEmpty (searchQuery)) {
+            if (!String.IsNullOrEmpty (searchQuery))
+            {
                 var searchResult = applicationDbContext.Where (p => p.Contractor.Name.Contains (searchQuery) || p.Contractor.NIP.Contains (searchQuery) || p.Number.Contains (searchQuery));
                 return View (await searchResult.ToListAsync ());
             }
 
-            if (month != null) {
+            if (month != null)
+            {
                 var filteredResult = applicationDbContext.Where (p => p.DateOfIssue.Month == month);
                 ViewData["Month"] = new SelectList (Tools.getMonthsDictionary (), "Key", "Value", month);
-                if (year != null) {
+                if (year != null)
+                {
                     filteredResult = applicationDbContext.Where (p => p.DateOfIssue.Month == month && p.DateOfIssue.Year == year);
                     ViewData["Year"] = new SelectList (Tools.getYearsList (), year);
                 }
                 return View (await filteredResult.ToListAsync ());
-            } else {
+            }
+            else
+            {
                 return View (await applicationDbContext.Where (p => p.DateOfIssue.Year == DateTime.Now.Year && p.DateOfIssue.Month == DateTime.Now.Month).ToListAsync ());
             }
 
             //return View(await applicationDbContext.ToListAsync());
         }
 
-        public IActionResult GetPdfFile (string filename, string downloadFilename) {
+        public IActionResult GetPdfFile (string filename, string downloadFilename)
+        {
             const string contentType = "application/pdf";
             HttpContext.Response.ContentType = contentType;
             FileContentResult result = null;
             filename = "tmp/" + filename;
 
-            try {
-                result = new FileContentResult (System.IO.File.ReadAllBytes (filename), contentType) {
+            try
+            {
+                result = new FileContentResult (System.IO.File.ReadAllBytes (filename), contentType)
+                {
                     FileDownloadName = downloadFilename + ".pdf"
                 };
                 Tools.deleteTempFiles (filename.Substring (4, 64));
                 return result;
-            } catch (FileNotFoundException e) {
+            }
+            catch (FileNotFoundException e)
+            {
                 Console.WriteLine (e.Message);
                 return NotFound ();
             }
         }
 
         // GET: GenerateInvoice        
-        public async Task<IActionResult> GenerateInvoice (int? id) {
-            if (id == null) {
+        public async Task<IActionResult> GenerateInvoice (int? id)
+        {
+            if (id == null)
+            {
                 return NotFound ();
             }
 
@@ -87,13 +103,15 @@ namespace firma_mvc.Controllers {
                 .SingleOrDefaultAsync (m => m.Id == id);
 
             invoice.InvoiceItems = _context.InvoiceItem.Where (p => p.InvoiceId == id).ToList ();
-            foreach (InvoiceItem invoiceItem in invoice.InvoiceItems) {
+            foreach (InvoiceItem invoiceItem in invoice.InvoiceItems)
+            {
                 //Item item = invoiceItem.Item;
                 //item.VAT = _context.VAT.Single(p => p.Id == item.VATId);
                 //item.UnitOfMeasure = _context.UnitOfMeasure.Single(p => p.Id == item.UnitOfMeasureId);
             }
 
-            if (invoice == null) {
+            if (invoice == null)
+            {
                 return NotFound ();
             }
             // to change
@@ -106,8 +124,10 @@ namespace firma_mvc.Controllers {
         }
 
         // GET: Invoice/Details/5
-        public async Task<IActionResult> Details (int? id) {
-            if (id == null) {
+        public async Task<IActionResult> Details (int? id)
+        {
+            if (id == null)
+            {
                 return NotFound ();
             }
 
@@ -119,15 +139,9 @@ namespace firma_mvc.Controllers {
                 .SingleOrDefaultAsync (m => m.Id == id);
 
             invoice.InvoiceItems = _context.InvoiceItem.Where (p => p.InvoiceId == id).ToList ();
-            foreach (InvoiceItem invoiceItem in invoice.InvoiceItems) {
-                //Item item = invoiceItem.Item;
-                //item.VAT = _context.VAT.Single(p => p.Id == item.VATId);
-                //item.UnitOfMeasure = _context.UnitOfMeasure.Single(p => p.Id == item.UnitOfMeasureId);
-            }
 
-            //invoice.TotalValue=invoice.get
-
-            if (invoice == null) {
+            if (invoice == null)
+            {
                 return NotFound ();
             }
 
@@ -135,7 +149,8 @@ namespace firma_mvc.Controllers {
         }
 
         // GET: Invoice/Create
-        public IActionResult Create () {
+        public IActionResult Create ()
+        {
             Invoice invoice = new Invoice ();
             invoice.Number = getNumber ();
             invoice.DateOfIssue = DateTime.Now;
@@ -152,16 +167,16 @@ namespace firma_mvc.Controllers {
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create ([Bind ("Id,Number,DateOfIssue,DateOfDelivery,ContractorId,PaymentMethodId")] Invoice invoice) {
+        public async Task<IActionResult> Create ([Bind ("Id,Number,DateOfIssue,DateOfDelivery,ContractorId,PaymentMethodId")] Invoice invoice)
+        {
             invoice.InvoiceStatusId = _context.InvoiceStatus.Single (p => p.Name == "nowa").Id;
             invoice.Paid = false;
-            //to change
             invoice.CompanyId = getCompanyId ();
 
-            if (ModelState.IsValid) {
+            if (ModelState.IsValid)
+            {
                 _context.Add (invoice);
                 await _context.SaveChangesAsync ();
-                //                return RedirectToAction(nameof(Index));
                 return RedirectToAction ("Details", "Invoices", new { id = invoice.Id });
             }
             ViewData["ContractorId"] = new SelectList (_context.Contractor, "Id", "Name", invoice.ContractorId);
@@ -170,13 +185,16 @@ namespace firma_mvc.Controllers {
         }
 
         // GET: Invoice/Edit/5
-        public async Task<IActionResult> Edit (int? id) {
-            if (id == null) {
+        public async Task<IActionResult> Edit (int? id)
+        {
+            if (id == null)
+            {
                 return NotFound ();
             }
 
             var invoice = await _context.Invoice.SingleOrDefaultAsync (m => m.Id == id);
-            if (invoice == null) {
+            if (invoice == null)
+            {
                 return NotFound ();
             }
             ViewData["ContractorId"] = new SelectList (_context.Contractor, "Id", "Name", invoice.ContractorId);
@@ -190,20 +208,29 @@ namespace firma_mvc.Controllers {
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit (int id, [Bind ("Id,Number,DateOfIssue,DateOfDelivery,ContractorId,PaymentMethodId,InvoiceStatusId,Paid")] Invoice invoice) {
-            if (id != invoice.Id) {
+        public async Task<IActionResult> Edit (int id, [Bind ("Id,Number,DateOfIssue,DateOfDelivery,ContractorId,PaymentMethodId,InvoiceStatusId,Paid")] Invoice invoice)
+        {
+            if (id != invoice.Id)
+            {
                 return NotFound ();
             }
 
-            if (ModelState.IsValid) {
-                try {
+            if (ModelState.IsValid)
+            {
+                try
+                {
                     invoice.CompanyId = getCompanyId ();
                     _context.Update (invoice);
                     await _context.SaveChangesAsync ();
-                } catch (DbUpdateConcurrencyException) {
-                    if (!InvoiceHeaderExists (invoice.Id)) {
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InvoiceHeaderExists (invoice.Id))
+                    {
                         return NotFound ();
-                    } else {
+                    }
+                    else
+                    {
                         throw;
                     }
                 }
@@ -216,8 +243,10 @@ namespace firma_mvc.Controllers {
         }
 
         // GET: Invoice/Delete/5
-        public async Task<IActionResult> Delete (int? id) {
-            if (id == null) {
+        public async Task<IActionResult> Delete (int? id)
+        {
+            if (id == null)
+            {
                 return NotFound ();
             }
 
@@ -225,7 +254,8 @@ namespace firma_mvc.Controllers {
                 .Include (i => i.Contractor)
                 .Include (i => i.PaymentMethod)
                 .SingleOrDefaultAsync (m => m.Id == id);
-            if (invoiceHeader == null) {
+            if (invoiceHeader == null)
+            {
                 return NotFound ();
             }
 
@@ -235,54 +265,68 @@ namespace firma_mvc.Controllers {
         // POST: Invoice/Delete/5
         [HttpPost, ActionName ("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed (int id) {
+        public async Task<IActionResult> DeleteConfirmed (int id)
+        {
             var invoiceHeader = await _context.Invoice.SingleOrDefaultAsync (m => m.Id == id);
             _context.Invoice.Remove (invoiceHeader);
             await _context.SaveChangesAsync ();
             return RedirectToAction (nameof (Index));
         }
 
-        private bool InvoiceHeaderExists (int id) {
+        private bool InvoiceHeaderExists (int id)
+        {
             return _context.Invoice.Any (e => e.Id == id);
         }
 
-        public string getNumber () {
+        public string getNumber ()
+        {
             string number = string.Empty;
             string month = DateTime.Now.Month.ToString ();
-            if (month.Length == 1) {
+            if (month.Length == 1)
+            {
                 month = month.Insert (0, "0");
             }
 
-            try {
+            try
+            {
                 string lastNumber = _context.Invoice.Last (p => p.DateOfIssue.Year == DateTime.Now.Year && p.DateOfIssue.Month == DateTime.Now.Month).Number;
                 int nextNumber = Int32.Parse (lastNumber.Substring (lastNumber.LastIndexOf ('/') + 1, lastNumber.Length - lastNumber.LastIndexOf ('/') - 1));
                 nextNumber++;
 
                 number = "FV/" + DateTime.Now.Year + "/" + month + "/" + nextNumber.ToString ();
-            } catch (Exception) {
+            }
+            catch (Exception)
+            {
                 number = "FV/" + DateTime.Now.Year + "/" + month + "/1";
             }
 
             return number;
         }
 
-        int countItems (int invoiceId) {
-            try {
+        int countItems (int invoiceId)
+        {
+            try
+            {
                 int count = _context.InvoiceItem.Where (p => p.InvoiceId == invoiceId).Count ();
                 Console.WriteLine (count);
                 return count;
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 return 0;
             }
         }
 
-        int getCompanyId () {
-            return _context.Company.Single (p => p.Name == "Computerman").Id;
+        int getCompanyId ()
+        {
+            return _context.Company.FirstOrDefault().Id;
         }
 
         // GET: Invoice/Confirm
-        public async Task<IActionResult> Confirm (int? id) {
-            if (id == null) {
+        public async Task<IActionResult> Confirm (int? id)
+        {
+            if (id == null)
+            {
                 return NotFound ();
             }
 
@@ -304,8 +348,10 @@ namespace firma_mvc.Controllers {
             sell.ValueBrutto = invoice.TotalValueInclVat;
 
             // TO DO: add other VAT rates support
-            foreach (InvoiceItem item in invoice.InvoiceItems) {
-                if (item.VATValue == 23) {
+            foreach (InvoiceItem item in invoice.InvoiceItems)
+            {
+                if (item.VATValue == 23)
+                {
                     sell.ValueNetto23 += item.TotalPrice;
                     sell.VATValue23 += item.TotalVATValue;
                 }
@@ -324,8 +370,16 @@ namespace firma_mvc.Controllers {
             _context.Update (invoice);
             await _context.SaveChangesAsync ();
 
-            //return RedirectToAction(nameof(Details), new { id = invoice.Id });
             return RedirectToAction (nameof (Index));
+        }
+
+        public async Task<IActionResult> SetPaid(int invoiceId)
+        {
+            var invoice = _context.Invoice.Single(p=>p.Id==invoiceId);
+            invoice.Paid=!invoice.Paid;
+            _context.Update (invoice);
+            await _context.SaveChangesAsync ();
+            return RedirectToAction (nameof (Details), new { id = invoiceId });
         }
     }
 }
